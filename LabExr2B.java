214fd2dca244de18eab616310475879746eb38a2
Kaleid <90696565+KaleidSkylark@users.noreply.github.com>
2021-10-16 20:50:26 +0800

    Create LabExr2B.java

---
+++ LabExr2B.java
@@ -0,0 +1,10 @@
+class LabExr2B {
+    
+    public static void main(String args[]){
+        LabExr2A shop = new LabExr2A();
+        shop.readInput();
+        shop.writeOutput();
+
+        
+            }
+    }
